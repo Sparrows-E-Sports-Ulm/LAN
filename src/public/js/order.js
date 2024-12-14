be6a67d8bef1ea0412b72b14d4cb2fa0b75d1726
snh21 <lars.pfrenger@uni-ulm.de>
2024-12-14 23:50:03 +0100

    com with server

--- src/public/js/order.js
+++ src/public/js/order.js
@@ -1,12 +1,72 @@
 "use strict";
 
-const format = new Intl.NumberFormat('de-DE', {style: 'currency', currency: 'EUR'}) 
+const format = new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' })
 const basket = [];
 const basketView = document.getElementById('basket');
+const nextButton = document.getElementById('next-btn');
+const searchInput = document.getElementById('search');
+const regisisteterView = document.getElementById('register');
+const regNextButton = document.getElementById('reg-next-btn');
 
+searchInput.oninput = () => {
+    const query = searchInput.value.toLowerCase();
+    const dishes = document.getElementsByClassName('dish');
 
-function addToBasket(category, dish, price) {
-    basket.push({category: category, dish: dish, price: price});
+    if (query.trim().length === 0) {
+        for (const element of dishes) {
+            element.style.display = '';
+        }
+        return;
+    }
+
+    for (const element of dishes) {
+        if (element.innerText.toLowerCase().includes(query)) {
+            element.style.display = '';
+        } else {
+            element.style.display = 'none';
+        }
+    }
+}
+
+nextButton.onclick = () => {
+    regisisteterView.style.display = '';
+}
+
+regNextButton.onclick = async () => {
+    regNextButton.disabled = true;
+    regNextButton.innerHTML = "...";
+
+    const body = basket.map(v => ({
+        category: v.catIndex,
+        dish: v.dishIndex
+    }));
+
+    const res = await fetch('/order/submit', {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    });
+
+    if (res.status === 200) {
+        window.location.href = '/order/status';
+    } else {
+        alert(`Es ist ein Fehler aufgetreten:\n${await res.text()}`);
+    }
+
+    regNextButton.innerHTML = "Weiter";
+    regNextButton.disabled = false;
+}
+
+document.getElementById('reg-back-btn').onclick = () => {
+    regisisteterView.style.display = 'none';
+    regNextButton.innerHTML = "Weiter";
+    regNextButton.disabled = false;
+}
+
+function addToBasket(catIndex, category, dishIndex, dish, price) {
+    basket.push({ catIndex: catIndex, category: category, dishIndex: dishIndex, dish: dish, price: price });
     const containerView = document.createElement('div');
     const nameView = document.createElement('span');
     const priceView = document.createElement('span');
@@ -17,6 +77,8 @@ function addToBasket(category, dish, price) {
     containerView.appendChild(nameView);
     containerView.appendChild(priceView);
     basketView.appendChild(containerView);
+
+    nextButton.innerText = `Weiter (${format.format(basket.reduce((acc, current) => acc + current.price, 0))})`;
 }
 
 function removeFromBasket(category, dish) {
--- src/routes/order.js
+++ src/routes/order.js
@@ -3,13 +3,17 @@ var router = express.Router();
 
 const menu = require('../models/vabene.json');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('order/order', menu);
 });
 
+router.post('/submit', function(req, res, next) {
+  console.log(JSON.stringify(req.body));
+  res.status(500).send('not implemented yet');
+});
+
 router.get('/status', function(req, res, next) {
     res.render('order/status');
-  });
+});
 
 module.exports = router;
