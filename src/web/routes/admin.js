2c831f5929f1c1bb9e68bf0f0ca7a636ccc5f957
snh21 <lars.pfrenger@uni-ulm.de>
2025-01-06 22:19:39 +0000

    feat(admin): download button for order list

--- src/web/routes/admin.js
+++ src/web/routes/admin.js
@@ -8,4 +8,41 @@ router.get('/', asyncRoute(async (req, res, next) => {
   res.render('admin/admin', { baskets: baskets });
 }));
 
+router.get('/download', asyncRoute(async (req, res, next) => {
+  const baskets = await BasketModel.find();
+
+  if(!baskets) {
+    res.status(500).send('No Baskets In DB');
+    return;
+  }
+
+  var output = `Bestellung vom ${new Date().toLocaleDateString('de')}\n\n`;
+
+  var productMap = {}; 
+
+  for(const basket of baskets) {
+    for(const item of basket.items) {
+      const entry = productMap[`${item.categoryId}-${item.dishId}`];
+      if(!entry) {
+        productMap[`${item.categoryId}-${item.dishId}`] = {
+          count: 1,
+          name: item.dish,
+          price: item.price
+        }
+
+        continue;
+      }
+
+      entry.count += 1;
+    }
+  }
+
+  for(item of Object.values(productMap)) {
+    output += `- ${item.count}x ${item.name}\n`
+  }
+
+  res.send(output);
+}));
+
+
 module.exports = router;
